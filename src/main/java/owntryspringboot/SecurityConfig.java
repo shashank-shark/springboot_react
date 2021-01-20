f6ee2e9b47491fd4a8c6c40279a7890fad5c58c7
Shashank J <shashankreddy231@gmail.com>
2021-01-20 10:50:06 +0530

    Added Spring Data Rest.

--- src/main/java/owntryspringboot/SecurityConfig.java
+++ src/main/java/owntryspringboot/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().and().authorizeRequests()
+
+        http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+
+        /* http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -34,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         UsernamePasswordAuthenticationFilter.class)
                 // Filter for other requests to check JWT in header
                 .addFilterBefore(new AuthenticationFilter(),
-                        UsernamePasswordAuthenticationFilter.class);
+                        UsernamePasswordAuthenticationFilter.class); */
     }
 
     @Bean
--- src/main/java/owntryspringboot/domain/CarRepository.java
+++ src/main/java/owntryspringboot/domain/CarRepository.java
@@ -2,15 +2,18 @@ package owntryspringboot.domain;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface CarRepository extends CrudRepository<Car, Long> {
-    @Query("SELECT c from Car c where c.brand=?1")
-    List<Car> findByBrand(String brand);
-
-    @Query("SELECT c from Car c where c.color=?1")
-    List<Car> findByColor(String color);
+@RepositoryRestResource(collectionResourceRel = "car", path = "cars")
+public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
+//    @Query("SELECT c from Car c where c.brand=?1")
+    List<Car> findByBrand(@Param("brand") String brand);
+//
+//    @Query("SELECT c from Car c where c.color=?1")
+    List<Car> findByColor(@Param("color")String color);
 }
